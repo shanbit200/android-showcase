a30064e9f62a9e50fd47d9b039fe61e3a6b64e3d
igor.wojda <igor.wojda@gmail.com>
2019-09-04 19:52:33 +0100

    updated plugin resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,9 @@ pluginManagement {
     // Add resolution strategy for gradle-android-junit-jacoco-plugin, so snapshot version can be properly resolved
     resolutionStrategy {
         eachPlugin {
+            println("requested.id.namespace: " + requested.id.namespace)
+            println("requested.version: " + requested.version)
+            println("--------")
             if (requested.id.namespace == "com.vanniktech.android.junit") {
                 useModule("com.vanniktech:gradle-android-junit-jacoco-plugin:${requested.version}")
             }
