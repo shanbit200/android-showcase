990d05f4bcbb789e265a624f7ed79db1aea1ae87
igor.wojda <igor.wojda@gmail.com>
2019-09-05 08:20:57 +0100

    Add onboardinf feature sample

--- buildSrc/src/main/kotlin/ModuleDependency.kt
+++ buildSrc/src/main/kotlin/ModuleDependency.kt
@@ -10,6 +10,7 @@ object ModuleDependency {
     const val FEATURE_ALBUM = ":feature_album"
     const val FEATURE_PROFILE = ":feature_profile"
     const val FEATURE_FAVOURITE = ":feature_favourite"
+    const val FEATURE_ONBOARDING = ":feature_onboarding"
     const val LIBRARY_BASE = ":library_base"
     const val LIBRARY_TEST_UTILS = ":library_test_utils"
 
---
+++ feature_onboarding/build.gradle.kts
@@ -0,0 +1,70 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+
+plugins {
+    id(GradlePluginId.ANDROID_DYNAMIC_FEATURE)
+    id(GradlePluginId.KOTLIN_ANDROID)
+    id(GradlePluginId.KOTLIN_ANDROID_EXTENSIONS)
+    id(GradlePluginId.SAFE_ARGS)
+}
+
+android {
+    compileSdkVersion(AndroidConfig.COMPILE_SDK_VERSION)
+
+    defaultConfig {
+        minSdkVersion(AndroidConfig.MIN_SDK_VERSION)
+        targetSdkVersion(AndroidConfig.TARGET_SDK_VERSION)
+
+        versionCode = AndroidConfig.VERSION_CODE
+        versionName = AndroidConfig.VERSION_NAME
+        testInstrumentationRunner = AndroidConfig.TEST_INSTRUMENTATION_RUNNER
+        vectorDrawables.useSupportLibrary = AndroidConfig.SUPPORT_LIBRARY_VECTOR_DRAWABLES
+    }
+
+    buildTypes {
+        getByName(BuildType.RELEASE) {
+            isMinifyEnabled = BuildTypeRelease.isMinifyEnabled
+            proguardFiles("proguard-android.txt", "proguard-rules.pro")
+        }
+
+        getByName(BuildType.DEBUG) {
+            isMinifyEnabled = BuildTypeDebug.isMinifyEnabled
+            isTestCoverageEnabled = true
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        // "this" is currently lacking a proper type
+        // See: https://youtrack.jetbrains.com/issue/KT-31077
+        val options = this as? KotlinJvmOptions
+        options?.jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    // This "test" source set is a fix for SafeArgs classes not being available when running Unit tests from cmd
+    // See: https://issuetracker.google.com/issues/139242292
+    sourceSets {
+        // Additionally due to ktlint gradle plugin bug we need one more workaround to disable Args class generation
+        // when ktlintCheck task is running
+        // See: https://github.com/JLLeitschuh/ktlint-gradle/issues/266
+        if (!gradle.startParameter.taskNames.contains("ktlintCheck")) {
+            getByName("test").java.srcDir("${project.rootDir}/app/build/generated/source/navigation-args/debug")
+        }
+    }
+
+    // Removes the need to mock need to mock classes that may be irrelevant from test perspective
+    testOptions {
+        unitTests.isReturnDefaultValues = TestOptions.IS_RETURN_DEFAULT_VALUES
+    }
+}
+
+androidExtensions { isExperimental = true }
+
+dependencies {
+    implementation(project(ModuleDependency.APP))
+
+    addTestDependencies()
+}
